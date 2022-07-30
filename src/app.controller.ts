538ccd88c57bcbfd576ec3b962d8b9f3d244b7c7
Jose Núñez Sandoval <joseenrique0595@gmail.com>
2022-07-30 03:53:32 -0600

    Adding endpoints for testing and getting information with swagger options

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 import { AppService } from './app.service';
+
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('test-selenium')
+  @ApiProperty({
+    description: 'Money',
+    type: Number
+  })
+  testSelenium(@Query('money') money: Number) {
+    this.appService.setupTest(money);
+    return true;
   }
+
+  @Get('get-properties')
+  @ApiProperty({
+    description: 'Money',
+    type: Number
+  })
+  getProperties(@Query('money') money: Number){
+    this.appService.initProcess(money);
+    return true;
+  }
+
 }
